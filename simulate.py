59e782f01683ddf842b6a79e52e4090de10f5739
simlouis <slouis@uvm.edu>
2022-01-19 11:09:41 -0500

    Started many links

--- simulate.py
+++ simulate.py
@@ -1,13 +1,17 @@
 import pybullet as p
 import time as t
+import pybullet_data
 
 physicsClient = p.connect(p.GUI)
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
 
+p.setGravity(0.0, 0.0, -9.8)
+planeId = p.loadURDF("plane.urdf")
 p.loadSDF("box.sdf")
 
 for i in range(1000):
     p.stepSimulation()
-    t.sleep(1 / 5)
+    t.sleep(1 / 60)
     print(i)
 
 p.disconnect()
