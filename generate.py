b5bcf27ac0dc8826311ee4358ef91125eaa8a27f
simlouis <slouis@uvm.edu>
2022-01-31 12:51:09 -0500

    Working on U shape

--- generate.py
+++ generate.py
@@ -7,16 +7,28 @@ x = 0
 y = 0
 z = 0.5
 
-pyrosim.Start_SDF("boxes.sdf")
 
-# pyrosim.Send_Cube(name="Box", pos=[x, y, z], size=[length, width, height])
+def create_world():
+    pyrosim.Start_SDF("world.sdf")
+    pyrosim.Send_Cube(name="Box", pos=[x - 2, y + 2, z], size=[length, width, height])
+    pyrosim.End()
 
-for i in range(1, 11):
-    length *= 0.9
-    width *= 0.9
-    height *= 0.9
-    for j in range(1, 5):
-        for k in range(1, 5):
-            pyrosim.Send_Cube(name="Box", pos=[x + j, y + k, z + i], size=[length, width, height])
 
-pyrosim.End()
+def create_robot():
+    pyrosim.Start_URDF("body.urdf")
+    pyrosim.Send_Cube(name="Link0", pos=[x, y, z], size=[length, width, height])
+    pyrosim.Send_Joint(name="Link0_Link1", parent="Link0", child="Link1", type="revolute", position=[0, 0, 1])
+    pyrosim.Send_Cube(name="Link1", pos=[x, y, z], size=[length, width, height])
+    pyrosim.Send_Joint(name="Link1_Link2", parent="Link1", child="Link2", type="revolute", position=[0, 0, 1])
+    pyrosim.Send_Cube(name="Link2", pos=[x, y, z], size=[length, width, height])
+    pyrosim.Send_Joint(name="Link2_Link3", parent="Link2", child="Link3", type="revolute", position=[0, 0.5, 0.5])
+    pyrosim.Send_Cube(name="Link3", pos=[x, y + 0.5, z - 0.5], size=[length, width, height])
+    pyrosim.Send_Joint(name="Link3_Link4", parent="Link3", child="Link4", type="revolute", position=[0, 1, 0])
+    pyrosim.Send_Cube(name="Link4", pos=[x, y + 0.5, z - 0.5], size=[length, width, height])
+    pyrosim.Send_Joint(name="Link4_Link5", parent="Link4", child="Link5", type="revolute", position=[0, 0.5, 0.5])
+    pyrosim.Send_Cube(name="Link5", pos=[x, y, z], size=[length, width, height])
+    pyrosim.End()
+
+
+create_world()
+create_robot()
--- simulate.py
+++ simulate.py
@@ -7,12 +7,12 @@ p.setAdditionalSearchPath(pybullet_data.getDataPath())
 
 p.setGravity(0.0, 0.0, -9.8)
 planeId = p.loadURDF("plane.urdf")
-p.loadSDF("boxes.sdf")
+robotId = p.loadURDF("body.urdf")
+p.loadSDF("world.sdf")
 
 for i in range(10000):
     p.stepSimulation()
     t.sleep(1 / 60)
-    # print(i)
 
 p.disconnect()
 
