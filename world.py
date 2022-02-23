dbdb8f38765c8cebcc5bb04ca698f4ce7ae5fce5
simlouis <slouis@uvm.edu>
2022-02-23 11:23:15 -0500

    Finished Refactoring

--- world.py
+++ world.py
@@ -4,6 +4,5 @@ import pybullet_data
 
 class WORLD:
     def __init__(self):
-        # p.setAdditionalSearchPath(pybullet_data.getDataPath())
         self.planeId = p.loadURDF("plane.urdf")
         p.loadSDF("world.sdf")
