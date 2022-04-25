ea2f35e0fe4d4034ae05a3e17f5477db5a54e521
simlouis <slouis@uvm.edu>
2022-04-25 16:19:34 -0400

    Plotted Means

--- plotFitnessValues.py
+++ plotFitnessValues.py
@@ -5,16 +5,20 @@ import constants as c
 A_data = numpy.load("A_data.npy")
 B_data = numpy.load("data.npy")
 
-A_data_mean = numpy.empty((0, c.numberOfGenerations))
+A_data_mean = []
+B_data_mean = []
 
-# for i in range(len(A_data)):
+for i in range(len(A_data)):
+    A_data_mean.append(numpy.mean(A_data[i]))
 
+for i in range(len(B_data)):
+    B_data_mean.append(numpy.mean(B_data[i]))
 
 pyplot.xlabel("Number of Generations")
 pyplot.ylabel("Fitness")
 
-pyplot.plot(A_data)
-# pyplot.plot(B_data)
+pyplot.plot(A_data_mean)
+pyplot.plot(B_data_mean)
 
 pyplot.legend(bbox_to_anchor=(0.75, 1))
 pyplot.show()
